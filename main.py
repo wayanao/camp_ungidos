810237a8291eacadf77f349d1b95ebe09db0815d
david_rodriguez <david21rodriguezserrano@gmail.com>
2026-04-20 12:45:07 -0500

    correccion

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ origins = [
     "http://127.0.0.1:5174",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://camp-ungidos.onrender.com"
+    "https://camp-ungidos.netlify.app"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
